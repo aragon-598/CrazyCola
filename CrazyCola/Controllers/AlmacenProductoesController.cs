9aec811c3664fa00f593816d1abef3264bd5b9a9
Alejandro Aragon <aragondru155@gmail.com>
2023-05-26 22:37:18 -0600

    Agregar archivos de proyecto.

---
+++ CrazyCola/Controllers/AlmacenProductoesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CrazyCola.Models;
+
+namespace CrazyCola.Controllers
+{
+    public class AlmacenProductoesController : Controller
+    {
+        private crazycolaEntities db = new crazycolaEntities();
+
+        // GET: AlmacenProductoes
+        public ActionResult Index()
+        {
+            var almacenProducto = db.AlmacenProducto.Include(a => a.Almacen).Include(a => a.Producto);
+            return View(almacenProducto.ToList());
+        }
+
+        // GET: AlmacenProductoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlmacenProducto almacenProducto = db.AlmacenProducto.Find(id);
+            if (almacenProducto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacenProducto);
+        }
+
+        // GET: AlmacenProductoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.AlmacenId = new SelectList(db.Almacen, "AlmacenId", "Nombre");
+            ViewBag.ProductoId = new SelectList(db.Producto, "ProductoId", "Nombre");
+            return View();
+        }
+
+        // POST: AlmacenProductoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AlmacenId,ProductoId,Stock")] AlmacenProducto almacenProducto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AlmacenProducto.Add(almacenProducto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AlmacenId = new SelectList(db.Almacen, "AlmacenId", "Nombre", almacenProducto.AlmacenId);
+            ViewBag.ProductoId = new SelectList(db.Producto, "ProductoId", "Nombre", almacenProducto.ProductoId);
+            return View(almacenProducto);
+        }
+
+        // GET: AlmacenProductoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlmacenProducto almacenProducto = db.AlmacenProducto.Find(id);
+            if (almacenProducto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AlmacenId = new SelectList(db.Almacen, "AlmacenId", "Nombre", almacenProducto.AlmacenId);
+            ViewBag.ProductoId = new SelectList(db.Producto, "ProductoId", "Nombre", almacenProducto.ProductoId);
+            return View(almacenProducto);
+        }
+
+        // POST: AlmacenProductoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AlmacenId,ProductoId,Stock")] AlmacenProducto almacenProducto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(almacenProducto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AlmacenId = new SelectList(db.Almacen, "AlmacenId", "Nombre", almacenProducto.AlmacenId);
+            ViewBag.ProductoId = new SelectList(db.Producto, "ProductoId", "Nombre", almacenProducto.ProductoId);
+            return View(almacenProducto);
+        }
+
+        // GET: AlmacenProductoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlmacenProducto almacenProducto = db.AlmacenProducto.Find(id);
+            if (almacenProducto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacenProducto);
+        }
+
+        // POST: AlmacenProductoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AlmacenProducto almacenProducto = db.AlmacenProducto.Find(id);
+            db.AlmacenProducto.Remove(almacenProducto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CrazyCola/Controllers/LoginController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrazyCola.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Login/Details/5
+        public ActionResult Enter(string email,string password)
+        {
+            try
+            {
+                using (Models.crazycolaEntities db = new Models.crazycolaEntities()  )
+                {
+                    var users = from d in db.Usuario where d.Email == email && d.Contrasenia == password select d;
+
+                    if (users.Count() > 0)
+                    {
+                        var user = users.First();
+                        Session["User"] = user;
+                    }
+                }
+
+                return Content("1");
+            }
+            catch(Exception ex)
+            {
+                return Content("Ocurrio un error =>"+ex.Message);
+            }
+            return View();
+        }
+
+        // GET: Login/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Login/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Login/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Login/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Login/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Login/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
